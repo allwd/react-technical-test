ae9206bfc3eea1f0ce112934da44fb83c7b6727c
allwd <slaczka.sebastian+allwd@gmail.com>
2019-05-22 21:22:06 +0200

    added ripple effect

--- src/Components/Menu/MenuItem.js
+++ src/Components/Menu/MenuItem.js
@@ -9,10 +9,20 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   user-select: none;
+  background-position: center;
+  transition: background 0.7s;
+  background-color: #ee5f63;
   
   &:hover {
     background: rgba(255, 255, 255, 0.2);
     cursor: pointer;
+    background: #ff6468 radial-gradient(circle, transparent 1%, #ff6468 1%) center/22000%;
+  }
+  
+  &:active {
+    background-color: #ee5f63;
+    background-size: 100%;
+    transition: background 0s
   }
 
   & + & {
