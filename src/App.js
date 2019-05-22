47d39d22f4b420ec65e7f96e8bbeec1f2a3cc82e
Sebastian Ślączka <slaczka.sebastian+allwd@gmail.com>
2019-05-22 22:12:01 +0200

    update default menu state

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const Content = styled.div`
 `
 
 const App = () => {
-  const [show, toggleMenu] = React.useState(true)
+  const [show, toggleMenu] = React.useState(false)
 
   return (
     <Wrapper>
